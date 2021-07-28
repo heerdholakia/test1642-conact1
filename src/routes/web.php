45927a2ca900bbd33f6ee49a5f2e699b543ca7c0
heer dholakia <heer.d@yudiz.in>
2021-07-28 09:13:04 +0530

    Iniial commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,22 +4,12 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Route;
 
+//composer require test1642/conact1
+
 Route::get('contact1',function(){
 
 	return "contact1";
 
 });
 
-// Route::get('contact',function(){
-
-// 	return view('contact::contact');
-// })->name('contact');
-
-
-// Route::post('contact',function(Request $request){
-// 	return $request->all();
-// });
-
-
-// Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
